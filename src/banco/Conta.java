6227a48b9da62aa92fc5f92ebe4092a7bb743248
Tulio Marcos Dias da Silva <tuliodias@yahoo.com.br>
2024-02-26 22:53:31 -0300

    commit 2 info F

--- src/banco/Conta.java
+++ src/banco/Conta.java
@@ -10,5 +10,49 @@ package banco;
  * @author tulio
  */
 public class Conta {
+
+    private int numero;  // atributos ou variáveis de instância
+    private String cliente;
+    private double saldo;
+    private double limite;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        
+        this.cliente = cliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
     
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public void setLimite(double limite) {
+        this.limite = limite;
+    }
+
+    
+        public void saca(double quantidade) { //método
+        this.saldo = this.saldo - quantidade;
+    }
+    public void deposita(double quantidade) { //método
+        this.saldo = this.saldo + quantidade;
+    }
+
 }
--- src/sintaxe/Operacoes.java
+++ src/sintaxe/Operacoes.java
@@ -10,5 +10,14 @@ package sintaxe;
  * @author tulio
  */
 public class Operacoes {
-    
+
+    double n1, n2, resultado; // variavel de instância ou um atributo
+
+    public double soma(double n1, double n2) { //métodos
+        this.n1 = n1;
+        this.n2 = n2;
+        this.resultado = this.n1 + this.n2;
+        return this.resultado;
+    }
+
 }
